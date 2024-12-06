39f596e6651dbb3a5474458c4d1c9a60f8630abe
Rishav Gayen <gayenrishav19@gmail.com>
2024-12-06 16:35:23 +0000

    Added script.js

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-const body = document.querySelector('body');
+const color = document.querySelector('#color');
+const buttons = document.querySelectorAll('.control');
+const range = document.querySelector('#range');
+const rangeLabel = document.querySelector('.range_label');
+const sketchBoard = document.querySelector('.panel__sketchboard');
+
+function generate_squares() {
+    pass;
+}
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        buttons.forEach((button) => button.classList.remove('active'));
+        button.classList.add('active');
+    })
+})
+
+
+range.addEventListener('change', () => {
+    rangeLabel.textContent = `${range.value}x${range.value}`;
+})
+
 
